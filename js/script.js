5f0eedbedb4bd6c8e7d65263c08c198e23b135c0
William Dias <smdwilliam89@gmail.com>
2025-05-23 16:32:42 -0300

    feat(script.js): Criando função para calcular a resposta correta.

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const perguntaEl = document.getElementById('pergunta');
+const respostasEl = document.getElementById('respostas');
+const cronometroEl = document.getElementById('cronometro');
+const feedbackEl = document.getElementById('feedback');
+
+let = 10;
+let timer;
+let respostaCorreta = 0;
+
+/**
+ * Função para calcular a resposta correta com base na operação
+ */
+function gerarPergunta() {
+    // 1.Gerar números aleatórios
+    const num1 = Math.floor(Math.random() * 10) + 1;
+    const num2 = Math.floor(Math.random() * 10) + 1;
+
+    // 2.Gerar operação aleatória
+    const operadores = ['+', '-', '*', '/'];
+    const operador = operadores[Math.floor(Math.random() * operadores.length)];
+
+    // 3.Gerar pergunta
+    let perguntaTexto = `${num1} ${operador} ${num2}`;
+    respostaCorreta = calcular(num1, num2, operador);
+
+    // 4. Atualizar no HTML
+    perguntaEl.textContent = perguntaTexto;
+
+    //5. Gerar respostas
+    gerarRespostas(respostaCorreta);
+
+    //6.Resetar feedback e cronômetro
+    feedbackEl.textContent = '';
+    iniciarCronometro();
+}
